946340a8040661648856c4aaa7f28afee5853fbb
soumen7saha <soumen7saha@gmail.com>
2025-12-22 10:04:03 +0530

    added Homework10

--- Module 10/homework_folder/q6_test.py
+++ Module 10/homework_folder/q6_test.py
@@ -2,8 +2,9 @@ import requests
 
 
 url = "http://localhost:9696/predict"
+url1 = "http://localhost:8080/predict"
 
 client = {"job": "management", "duration": 400, "poutcome": "success"}
-response = requests.post(url, json=client).json()
+response = requests.post(url1, json=client).json()
 
 print(response)
